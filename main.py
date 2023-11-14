ea6384e261486dcb4fff822b47678dc0c26d1dc9
あかあく <105225777+AkaakuHub@users.noreply.github.com>
2023-11-14 23:51:39 +0900

    Update main.py
    ・n項ゴママヨを追加
    ・発音を正しくした

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ def judge_gomamayo(content):
   mecab = MeCab.Tagger()
   m_result = mecab.parse(content).splitlines()[:-1]
   pronounce = []
+  goma_idx = []
   for line in m_result:
     pro = line.split("\t")[1]
-    alk = alkana.get_kana(pro.lower())
+    read = line.split("\t")[2]
+    part = line.split("\t")[4]
+    # 「今日は」->「きょうわ」となるようにする
+    # 基本はreadを採用だが、「は」を「wa」と呼ぶときは変える
+    word = pro if "助詞" in part else read
+    alk = alkana.get_kana(word.lower())
     if len(pro) != 0:
-      pronounce.append(alk if alk != None else pro)
-    # ☆等は読みが空白のため除去
- 
+      pronounce.append(alk if alk != None else word)
+    # 記号等は読みが空白のため除去
+
   res = "違います。"
 
   for i in range(len(pronounce) - 1):
@@ -19,13 +25,31 @@ def judge_gomamayo(content):
     for j in range(min(len(pronounce[i]), len(pronounce[i + 1]))-1):
       if pronounce[i][-2 - j : -1] == pronounce[i + 1][0 : j + 1]:
         res = "高次ゴママヨです。"
+        goma_idx.append(i+0.5)
         break
-        
+
     # 低次ゴママヨ
     if pronounce[i][-1] == pronounce[i+1][0]:
       res = "ゴママヨです。"
-  
+      goma_idx.append(i+0.5)
+
+  # n項ゴママヨの判定
+  if len(goma_idx) > 1:
+    nominal = 1
+    max_nominal = 1
+    for i in range(len(goma_idx) - 1):
+      if goma_idx[i] + 1 == goma_idx[i+1]:
+        nominal += 1
+      else:
+        max_nominal = max(max_nominal, nominal)
+        nominal = 1
+    
+    max_nominal = max(max_nominal, nominal)
+
+    if max_nominal > 1:
+      res = f"{max_nominal}項ゴママヨです。"
+
   return (res, pronounce)
 
 if __name__ == '__main__':
-  print(judge_gomamayo('ガーリック食った')) # => ('ゴママヨです。', ['ガーリック', 'クッ', 'タ'])
+  print(judge_gomamayo('太鼓公募募集終了'))
